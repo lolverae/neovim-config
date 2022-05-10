1b70cb3a4b4c61f1822981a68b82d322f8371a10
Alberto <lolverae@github.com>
2022-05-09 23:20:55 -0500

    Add better emojis

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -2,10 +2,10 @@ local M = {}
 
 M.setup = function()
   local signs = {
-    { name = "DiagnosticSignError", text = "☢" },
-    { name = "DiagnosticSignWarn", text = "⚠" },
-    { name = "DiagnosticSignHint", text = "⁉" },
-    { name = "DiagnosticSignInfo", text = "ℹ" },
+    { name = "DiagnosticSignError", text = "☢️" },
+    { name = "DiagnosticSignWarn", text = "⚠️" },
+    { name = "DiagnosticSignHint", text = "❓" },
+    { name = "DiagnosticSignInfo", text = "ℹ️" },
   }
 
   for _, sign in ipairs(signs) do
